2817fb9434e129274657d43cef6c800fc41f7ba3
FabianJuarez182 <jua21440@uvg.edu.gt>
2022-02-26 18:13:44 -0600

    Creacion de las clases y utilizacion de vista

---
+++ Logicas.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C), 2022-2023, The_Kiesling FabianJuarez SaraEcheverria
+ * FileName: View
+ * Author:   Fabian Juarez , Sara Echeverria , Jose Pablo Kiesling
+ * Date:     26/02/2022
+ * @author Jose Pablo Kiesling, Fabian Juarez y Sara Echeverria
+ * @version:
+        - Creacion: 26/02/2022
+        - Ultima modificacion:
+    Clase que tiene como fin ser un
+ */
+public class Logicas {
+    
+}
---
+++ Scanner.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C), 2022-2023, The_Kiesling FabianJuarez SaraEcheverria
+ * FileName: View
+ * Author:   Fabian Juarez , Sara Echeverria , Jose Pablo Kiesling
+ * Date:     26/02/2022
+ * @author Jose Pablo Kiesling, Fabian Juarez y Sara Echeverria
+ * @version:
+        - Creacion: 26/02/2022
+        - Ultima modificacion:
+    Clase que tiene como fin ser un lector y escritura de datos
+ */
+import java.util.ArrayList;
+public class Scanner {
+    ArrayList<String> tokens;
+    /**
+     * @param mensaje
+     */
+    public void escribir(String mensaje){
+        System.out.println(mensaje);
+    }
+}
