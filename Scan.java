00c1c7fa1c4efc477fafbbad16ed7c375cce2773
FabianJuarez182 <jua21440@uvg.edu.gt>
2022-02-26 20:04:56 -0600

    Scanner que posee solamente 2 operaciones implementadas para ser leidas

---
+++ Scan.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (C), 2022-2023, The_Kiesling FabianJuarez SaraEcheverria
+ * FileName: View
+ * Author:   Fabian Juarez , Sara Echeverria , Jose Pablo Kiesling
+ * Date:     26/02/2022
+ * @author Jose Pablo Kiesling, Fabian Juarez y Sara Echeverria
+ * @version:
+        - Creacion: 26/02/2022
+        - Ultima modificacion:
+    Clase que tiene como fin ser un lector y escritura de datos
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Scan {
+    ArrayList<String> tokens;
+    //---------------------------METODOS------------------------------
+    /*****************************************************************
+    /**
+     * @param mensaje
+     */
+    public void escribir(String mensaje){
+        System.out.println(mensaje);
+    }
+    //****************************************************************
+
+    /*****************************************************************
+    /**
+     * @return lectura
+     */
+    public String leer(){
+        Scanner sc = new Scanner(System.in);
+        String lectura = sc.nextLine();
+
+        return lectura;
+
+    }
+    //****************************************************************
+
+    /*****************************************************************
+    /***
+	 * retornara que operacion se debera efectuar segun su numero
+	 * @param daroEvaluar nos dara la expresion que se evaluara
+	 * @return un numero que si es distinto a -1 y mayor que  que indicara que operacion se esta realizando
+	 */
+    public int obtenerTipo(String datoEvaluar){
+		if (evaluar("^[(][ ]*setq[ ]+[a-z]+[ ]+[0-9]+[ ]*[)]$",datoEvaluar)) //Asignacion que vera si esta es un set de la variable
+			return 1;
+		else if (evaluar("^[(][ ]*[+][ ]+([a-z]+|[0-9]+)[ ]+([a-z]+|[0-9]+)[ ]+[ ]*[)]$",datoEvaluar)) //Realizara la operacion de sumatoria para 2 operandos
+			return 2;
+		else
+			return -1; //De no encontrar la expresion dara  este dato para que sea incorrecta
+	}
+    //****************************************************************
+
+    /*****************************************************************
+    /**
+     * metodo que verificara si es una expresion o no
+	 * @param regex regla a seguir(formato regex)
+	 * @param expresion La expresion que nos seran brindados por el usuario
+	 * @return si es verdadero es que si hay alguna operacion a evaluar, si es falso no habra ninguna
+	 */
+	private boolean evaluar(String regex, String expresion) {
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(expresion);
+        return matcher.find();
+	}
+    //****************************************************************
+}
