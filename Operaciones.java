3eb27dbd23206b78317c016f2b77aeabfff6c00b
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-02-26 19:02:25 -0600

    Operaciones class.

---
+++ Operaciones.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C), 2022-2023, The_Kiesling FabianJuarez182 bl33h
+ * FileName: Operaciones
+ * Author:   Fabian Juarez , Sara Echeverria , Jose Pablo Kiesling
+ * Date:     26/02/2022
+ * @author Jose Pablo Kiesling, Fabian Juarez y Sara Echeverria
+ * @version:
+        - Creacion: 26/02/2022
+        - Ultima modificacion:
+ */
+public class Operaciones {
+    
+
+}
