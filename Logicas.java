786e8b8c1a39969abeba67f7a2a24224c4f1d7de
FabianJuarez182 <jua21440@uvg.edu.gt>
2022-03-16 18:15:11 -0600

    Creacion de metodos: equals, mayor que y menor que

--- Logicas.java
+++ Logicas.java
@@ -9,6 +9,46 @@
         - Ultima modificacion:
     Clase que tiene como fin ser un
  */
-public class Logicas {
+public class Logicas<T>{
     
+    /*****************************************************************
+     * Metodo que realizara la igualacion de los parametros ingresados
+     * @param valor Uno de los parametros a comparar
+     * @param valor2 Segundo parametro que se compara
+     * @return comparacion de si es verdadero o no la igualacion
+     */
+    public boolean equals(T valor, T valor2){
+        return valor.equals(valor2);
+    }
+    //****************************************************************
+    
+        /*****************************************************************
+     * Metodo que realizara la verificacion si el numero es menor que el otro
+     * @param valor Uno de los parametros a comparar
+     * @param valor2 Segundo parametro que se compara
+     * @return comparacion de si es verdadero o no la operacion realizada
+     */
+    public Boolean menorComparar(int valorA, int valorB ){
+        if(valorA < valorB){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    //****************************************************************
+
+            /*****************************************************************
+     * Metodo que realizara la verificacion si el numero es mayor que el otro
+     * @param valor Uno de los parametros a comparar
+     * @param valor2 Segundo parametro que se compara
+     * @return comparacion de si es verdadero o no la operacion realizada
+     */
+    public Boolean mayorComparar(int valorA, int valorB ){
+        if(valorA > valorB){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    //****************************************************************
 }
