1b721bd9175208f7c8ea1d996a25befbe3d6d311
FabianJuarez182 <jua21440@uvg.edu.gt>
2022-03-24 18:55:43 -0600

    Junit de 1 variable y de 2 variables

--- JunitTest.java
+++ JunitTest.java
@@ -14,35 +14,192 @@ class JunitTest {
 	private Aritmeticas calc = new Aritmeticas();
 	private Logicas logic = new Logicas();
 	private Interprete inter = new Interprete();
-	
+	private ArrayList<String> expresiones = new ArrayList<String>();	
+	private ArrayList<ArrayList<String>> tokens = new ArrayList<ArrayList<String>>();
+	private Scan sc = new Scan();
+	private String variable;
+
 	@Test
 	void test_Suma() {
 		assertEquals(12, calc.Evaluate("+ 4 8"));
 	}
 
 	@Test
+	void test_Suma_Var() {
+		expresiones.add("(setq var 10)");
+		expresiones.add("(+ 4 var)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
+
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("14", variable.toString());
+	}
+	
+	@Test
+	void test_Suma_Var2() {
+		expresiones.add("(setq var 10)");
+		expresiones.add(("setq var2 10"));
+		expresiones.add("(+ var2 var)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
+
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("20", variable.toString());
+	}
+	
+	@Test
 	void test_Resta() {
 		assertEquals(-20, calc.Evaluate("- 28 8"));
 	}
 
+	@Test
+	void test_Resta_Var() {
+		expresiones.add("(setq var 10)");
+		expresiones.add("(- 4 var)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
 
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("6", variable.toString());
+	}
+
+	@Test
+	void test_Resta_Var2() {
+		expresiones.add("(setq var 10)");
+		expresiones.add(("setq var2 10"));
+		expresiones.add("(- var2 var)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
+
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("0", variable.toString());
+	}
+	
 	@Test
 	void test_Division() {
 		assertEquals(14, calc.Evaluate("/ 10 140"));
 	}
 
+	@Test
+	void test_Division_Var() {
+		expresiones.add("(setq var 10)");
+		expresiones.add("(/ var 140)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
 
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("14", variable.toString());
+	}
+	
+	@Test
+	void test_Division_Var2() {
+		expresiones.add("(setq var 10)");
+		expresiones.add(("setq var2 10"));
+		expresiones.add("(/ var2 var)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
+
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("1", variable.toString());
+	}
+	
 	@Test
 	void test_Multiplicacion() {
 		assertEquals(10, calc.Evaluate("* 5 2"));
 	}
 
+	@Test
+	void test_Multiplicacion_Var() {
+		expresiones.add("(setq var 5)");
+		expresiones.add("(* var 2)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
 
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("10", variable.toString());
+	}
+	
+	@Test
+	void test_Multiplicacion_Var2() {
+		expresiones.add("(setq var 10)");
+		expresiones.add(("setq var2 10"));
+		expresiones.add("(* var2 var)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
+
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("100", variable.toString());
+	}
+	
 	@Test
 	void test_Quote() {
 		assertEquals("5", inter.quote("' 5"));
 	}
+	
+	@Test
+	void test_Quote_Var() {
+		expresiones.add("(setq var 5)");
+		expresiones.add("(' var)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
 
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("5", variable.toString());
+	}
 	
 	@Test
 	void test_Defun() {
@@ -59,6 +216,7 @@ class JunitTest {
 		assertEquals("T", logic.isAtom("Atom x"));
 	}
 	
+	
 	@Test
 	void test_List() {
 		assertEquals("T", logic.isList("List (1 2 3 4 5 6 7 8 9 10)"));
@@ -70,13 +228,118 @@ class JunitTest {
 	}
 	
 	@Test
+	void test_Equal_Var() {
+		expresiones.add("(setq var 5)");
+		expresiones.add("(equals var 2)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
+
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("Falso", variable.toString());
+	}
+	
+	@Test
+	void test_Equal_Var2() {
+		expresiones.add("(setq var 10)");
+		expresiones.add(("setq var2 10"));
+		expresiones.add("(equals var2 var)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
+
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("verdadero", variable.toString());
+	}	
+	
+	@Test
 	void test_Mayor() {
 		assertEquals("verdadero", logic.mayorComparar("> 10 5"));
 	}
 	
 	@Test
+	void test_Mayor_Var() {
+		expresiones.add("(setq var 5)");
+		expresiones.add("(> var 2)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
+
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("verdadero", variable.toString());
+	}
+	
+	@Test
+	void test_Mayor_Var2() {
+		expresiones.add("(setq var 10)");
+		expresiones.add(("setq var2 0"));
+		expresiones.add("(> var2 var)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
+
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("Falso", variable.toString());
+	}
+	
+	@Test
 	void test_Menor() {
 		assertEquals("Falso", logic.menorComparar("< 10 5"));
+	}
+	
+	@Test
+	void test_Menor_Var() {
+		expresiones.add("(setq var 5)");
+		expresiones.add("(< var 2)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
+
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("Falso", variable.toString());
+	}
+	
+	@Test
+	void test_Menor_Var2() {
+		expresiones.add("(setq var 10)");
+		expresiones.add(("setq var2 0"));
+		expresiones.add("(< var2 var)");
+		for (String s: expresiones)
+            tokens.add(sc.tokens(s));
+
+        //Evaluate
+        for (ArrayList<String> al: tokens){
+            String expresion = "";
+            for (String s: al)
+                expresion += s + " ";
+            variable = inter.operate(al, sc.obtenerTipo(al));
+        }
+		assertEquals("Falso", variable.toString());
 	}
 	
 	@Test
